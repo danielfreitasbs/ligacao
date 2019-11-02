3eef46a15c5e6615b76ef8d4cc7ec2dfd2f9ed25
Alice Francener <alicefrancener@gmail.com>
2019-11-02 19:42:51 -0300

    refactor: modifica métodos

--- src/main/java/br/com/ligacao/client/apps/AcaoApp.java
+++ src/main/java/br/com/ligacao/client/apps/AcaoApp.java
@@ -1,65 +1,45 @@
 package br.com.ligacao.client.apps;
 
+import br.com.ligacao.persistence.model.Acao;
 import br.com.ligacao.persistence.model.Promotor;
-br.com.ligacao.persistence.model
-import java.io.IOException;
+
 import java.util.Scanner;
-import java.util.concurrent.ExecutionException;
 
-public class CadastrarAcaoApp {
+/**
+ * Classe responsavel por gerenciar metodos relacionados a acoes.
+ */
+public class AcaoApp {
 
-    private static Scanner scanner;
+    private static Promotor promotor = new Promotor();
+    private static Acao acao = new Acao();
+    private static Scanner scanner = new Scanner(System.in);;
 
-    public static void main(String[] args) throws InterruptedException,
-            ExecutionException, IOException {
-        scanner = new Scanner(System.in);
+    public static void simuladorInterface(){
 
-        System.out.println("----- CADASTRO DE AÇÃO -----");
-        System.out.println("Nome do promotor: ");
-        String nomePromotor = scanner.nextLine();
+        System.out.println("----- O que deseja executar? -----\n"
+                        + "0 - Cadastrar Ação.\n"
+                        + "1 - Editar Ação\n"
+                        + "2 - Cancelar\n");
 
-        Promotor promotor = new Promotor();
-        promotor = Database.consultaPromotor(nomePromotor);
-        if( promotor == null) {
-            System.out.println("Promotor não está registrado no banco de " +
-                    "dados. Fazer registro de Promotor antes de registrar " +
-                    "ação.");
-            scanner.close();
-            System.exit(0);
-        }
-
-        System.out.println("Nome da ação: ");
-        String nomeAcao = scanner.nextLine();
-
-        System.out.println("Categoria da ação: ");
-        String categoria = scanner.nextLine();
-
-        System.out.println("Data de realização: ");
-        String data= scanner.nextLine();
-
-        System.out.println("Descrição: ");
-        String descricao = scanner.nextLine();
-
-        System.out.println("Horário de início: ");
-        String horaInicio = scanner.nextLine();
-
-        System.out.println("Horário de fim: ");
-        String horaFim = scanner.nextLine();
-
-        System.out.println("\nDigite 1 para confirmar o cadastro da acao ou 0" +
-                " para sair sem cadastrar.\n");
         int opcao = scanner.nextInt();
 
-        if (opcao == 1) {
-            scanner.close();
-            Database.cadastraAcao(nomePromotor, nomeAcao, categoria, data,
-                    descricao, horaInicio, horaFim);
-            System.exit(0);
-        } else {
-            scanner.close();
-            System.exit(0);
+        switch (opcao) {
+            case 0:
+                cadastrar();
+                break;
+            case 1:
+                editar();
+                break;
+            default:
+                break;
         }
-
     }
 
+    private static void editar() {
+        //TODO
+    }
+
+    private static void cadastrar() {
+        //TODO
+    }
 }
