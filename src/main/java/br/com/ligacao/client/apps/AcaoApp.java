ec1564f4dec0be45f86e3e78e0c2421754ec8f79
Alice Francener <alicefrancener@gmail.com>
2019-11-05 17:15:56 -0300

    feat: implementa método de edição de ações

--- src/main/java/br/com/ligacao/client/apps/AcaoApp.java
+++ src/main/java/br/com/ligacao/client/apps/AcaoApp.java
@@ -7,6 +7,7 @@ import br.com.ligacao.persistence.model.Promotor;
 import br.com.ligacao.persistence.model.PromotorDAO;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.ExecutionException;
 
@@ -66,6 +67,13 @@ public class AcaoApp {
         }
     }
 
+    /**
+     * Metodo responsavel por cadastrar nova acao
+     *
+     * @throws ExecutionException   se houver problema...
+     * @throws InterruptedException se houver problema...
+     * @throws IOException          se houver problema na entrada
+     */
     private static void cadastrar() throws ExecutionException, InterruptedException, IOException {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Nome do promotor: ");
@@ -94,38 +102,133 @@ public class AcaoApp {
                     } else simuladorInterface(null);
                 }
             }
-                System.out.println("Categoria da ação: ");
-                acao.setCategoriaAcao(scanner.nextLine());
+            System.out.println("Categoria da ação: ");
+            acao.setCategoriaAcao(scanner.nextLine());
 
-                System.out.println("Descrição: ");
-                acao.setDescricao(scanner.nextLine());
+            System.out.println("Descrição: ");
+            acao.setDescricao(scanner.nextLine());
 
-                System.out.println("Horário de início: ");
-                acao.setHoraInicio(scanner.nextLine());
+            System.out.println("Horário de início: ");
+            acao.setHoraInicio(scanner.nextLine());
 
-                System.out.println("Horário de fim: ");
-                acao.setHoraFim(scanner.nextLine());
+            System.out.println("Horário de fim: ");
+            acao.setHoraFim(scanner.nextLine());
 
-                System.out.println("0 - Confirmar o cadastro da acao\n"
-                        + "1 - Cancelar.\n");
-                int opcaoConfirmar = scanner.nextInt();
+            System.out.println("0 - Confirmar o cadastro da acao\n"
+                    + "1 - Cancelar.\n");
+            int opcaoConfirmar = scanner.nextInt();
 
-                if (opcaoConfirmar == 0) {
-                    try {
-                        AcaoDAO.cadastraAcao(promotor, acao);
-                        System.out.println("Acão cadastrada com sucesso!");
-                    } catch (IOException e) {
-                        System.out.println("Erro ao cadastrar Ação!");
-                        e.printStackTrace();
-                    }
-                } else {
-                    System.out.println("Cadastro da Ação cancelado!");
+            if (opcaoConfirmar == 0) {
+                try {
+                    AcaoDAO.cadastraAcao(promotor, acao);
+                    System.out.println("Acão cadastrada com sucesso!");
+                } catch (IOException e) {
+                    System.out.println("Erro ao cadastrar Ação!");
+                    e.printStackTrace();
                 }
+            } else {
+                System.out.println("Cadastro da Ação cancelado!");
+            }
         }
     }
 
-    private static void editar() {
-        //TODO
+    /**
+     * Metodo responsavel por editar acao de um promotor existente
+     *
+     * @throws ExecutionException   se houver problema...
+     * @throws InterruptedException se houver problema...
+     * @throws IOException          se houver problema na entrada
+     */
+    private static void editar() throws ExecutionException, InterruptedException, IOException {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Nome do promotor:");
+        String nomePromotor = scanner.nextLine();
+        promotor = PromotorDAO.consultaPromotor(nomePromotor);
+        if (promotor == null) {
+            System.out.println("Esse promotor não existe.");
+            simuladorInterface(null);
+        }
+
+        List<Acao> acoes = AcaoDAO.consultaAcoes(nomePromotor);
+        if (acoes.size() == 0) {
+            System.out.println("Esse promotor não possui ações cadastradas.");
+            simuladorInterface(null);
+        }
+        System.out.println("Esse promotor possui as seguintes ações:");
+        acoes.forEach(acao -> System.out.println(acao.getNomeAcao()));
+
+        System.out.println("Nome da ação a ser editada:");
+        String nomeAcao = scanner.nextLine();
+        acao = AcaoDAO.consultaAcao(nomePromotor, nomeAcao);
+
+        if (acao == null) {
+            System.out.println("Essa ação não existe.\n");
+        } else {
+            int opcao = 0;
+            while (opcao > -1 && opcao < 6) {
+                System.out.println("Qual campo gostaria de editar:\n"
+                        + "0 - Categoria\n"
+                        + "1 - Data de realização\n"
+                        + "2 - Descrição\n"
+                        + "3 - Hora de início\n"
+                        + "4 - Hora de fim\n"
+                        + "5 - Nome da ação\n"
+                        + "6 - Voltar ao menu inicial de ações\n");
+                opcao = scanner.nextInt();
+
+                switch (opcao) {
+                    case 0:
+                        System.out.println("Categoria atual: "
+                                + acao.getCategoriaAcao()
+                                + "\nModificar para:");
+                        scanner.nextLine();
+                        String modificado = scanner.nextLine();
+                        acao.setCategoriaAcao(modificado);
+                        break;
+                    case 1:
+                        System.out.println("Data de realização atual: "
+                                + acao.getDataRealizacao()
+                                + "\nModificar para:");
+                        acao.setDataRealizacao(scanner.nextLine());
+                        break;
+                    case 2:
+                        System.out.println("Descrição atual: "
+                                + acao.getDescricao()
+                                + "\nModificar para:");
+                        acao.setDescricao(scanner.nextLine());
+                        break;
+                    case 3:
+                        System.out.println("Hora de início atual: "
+                                + acao.getHoraInicio()
+                                + "\nModificar para:");
+                        acao.setHoraInicio(scanner.nextLine());
+                        break;
+                    case 4:
+                        System.out.println("Hora de fim atual: "
+                                + acao.getHoraFim()
+                                + "\nModificar para:");
+                        acao.setHoraFim(scanner.nextLine());
+                        break;
+                    case 5:
+                        System.out.println("Nome da Ação atual: "
+                                + acao.getNomeAcao()
+                                + "\nModificar para:");
+                        acao.setNomeAcao(scanner.nextLine());
+                        break;
+                    default:
+                        simuladorInterface(null);
+                        break;
+                }
+                try {
+                    AcaoDAO.cadastraAcao(promotor, acao);
+                    System.out.println("Ação editada com sucesso!");
+                } catch (IOException e) {
+                    System.out.println("Erro ao salvar ação.");
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     private static void excluir() {
