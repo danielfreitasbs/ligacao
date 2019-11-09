79a9b7765ad5e807c620d679b3da94ba9a54ee41
Alice Francener <alicefrancener@gmail.com>
2019-11-09 18:43:07 -0300

    feat(model): interface para login

--- src/main/java/br/com/ligacao/persistence/model/ILogin.java
+++ src/main/java/br/com/ligacao/persistence/model/ILogin.java
@@ -1,4 +1,9 @@
 package br.com.ligacao.persistence.model;
 
 public interface ILogin {
+
+    boolean logarPromotor(String usuario, String senha);
+
+    boolean logarVoluntario(String usuario, String senha);
+
 }
