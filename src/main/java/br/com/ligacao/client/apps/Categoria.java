253844d14d046e59af3d97691d538985f42d320b
Alice Francener <alicefrancener@gmail.com>
2019-11-20 20:56:09 -0300

    feat: cria enum com categorias de acao

---
+++ src/main/java/br/com/ligacao/client/apps/Categoria.java
@@ -0,0 +1,14 @@
+package br.com.ligacao.client.apps;
+
+/**
+ * Possiveis categorias de uma acao.
+ */
+public enum Categoria {
+        social,
+        ambiental,
+        animal,
+        cultural,
+        educativa,
+        esportiva,
+        artistica
+}
