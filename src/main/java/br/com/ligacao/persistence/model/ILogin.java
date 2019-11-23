699e01395b9adc23a7cba502be8f4995e7b5891d
Alice Francener <alicefrancener@gmail.com>
2019-11-23 12:19:36 -0300

    del: excluir arquivos desnecessários

--- src/main/java/br/com/ligacao/persistence/model/ILogin.java
+++
@@ -1,9 +0,0 @@
-package br.com.ligacao.persistence.model;
-
-public interface ILogin {
-
-    boolean logarPromotor(String usuario, String senha);
-
-    boolean logarVoluntario(String usuario, String senha);
-
-}
