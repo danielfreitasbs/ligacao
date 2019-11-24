9fbda1a11205b4ed379f526ab8bf74b110d6ec03
Octavio Lo Leggio <ottoleggio@gmail.com>
2019-11-24 09:45:39 -0200

    testes de consultas promotor e login

--- src/test/java/br/com/ligacao/persistence/model/DAOtest.java
+++ src/test/java/br/com/ligacao/persistence/model/DAOtest.java
@@ -10,16 +10,25 @@ import org.junit.jupiter.api.Test;
 class DAOtest {
 
     @Test
-    void test() throws IOException, InterruptedException, ExecutionException {
+    void testPromotorDAO() throws IOException, InterruptedException, ExecutionException {
         PromotorFisico promotorFisicoTeste = new PromotorFisico("userTeste", "senhaTeste", "nomeTeste", 
                 "325", "31/12/2099", "img", "http", "categoriaTeste", "descricaoTeste", 
                 "12341234", "av. teste", "teste@teste");
         //cadastra promotor no banco firestore
         PromotorDAO.cadastraPromotorFisico(promotorFisicoTeste);
+        
         //testa o tipo de promotor cadastrado
         assertEquals(PromotorDAO.consultaTipoPromotor("nomeTeste"),
                 "fisica");
         
+        //testa consulta de promotor fisico cadastrado
+        PromotorFisico promotorFisicoTesteRetorno = PromotorDAO.retornaPromotorFisico("nomeTeste");
+        assertEquals(promotorFisicoTesteRetorno.getNomePessoa(),"nomeTeste");
+                
+        //testa consulta de login de promotor fisico cadastrado
+        PromotorFisico promotorFisicoTesteLogin = PromotorDAO.loginPromotorFisico("userTeste", "senhaTeste");
+        assertEquals(promotorFisicoTesteLogin.getNomePessoa(),"nomeTeste");
+                
         //exclui cadastro de teste no banco firestore
         PromotorDAO.excluiPromotor("nomeTeste");
     }
