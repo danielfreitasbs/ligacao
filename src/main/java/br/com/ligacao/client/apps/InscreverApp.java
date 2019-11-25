3813fff7c4a358030ad9ae2d729623c4c7cb71ad
Caio Garcia Saraiva <caiobas6@hotmail.com>
2019-11-24 23:12:24 -0300

    docs: fix docs InscreverApp

--- src/main/java/br/com/ligacao/client/apps/InscreverApp.java
+++ src/main/java/br/com/ligacao/client/apps/InscreverApp.java
@@ -11,34 +11,45 @@ import br.com.ligacao.services.InscreverService;
 /**
  * Classe responsável pela inscrição de um voluntário em uma ação de seu interesse.
  */
-public class InscreverApp {
-	
+public final class InscreverApp {
+
+	/**
+     * Construtor privado para evitar instanciação da classe.
+     */
+    private InscreverApp() {
+    }
+
 	/**
      * Scanner para entrada de dados pelo usuario.
      */
     private static Scanner scanner = new Scanner(System.in);
-    
-	/**
-	 * Método de execução da inscrição do voluntário em uma ação de interesse.
-	 * 
-	 * @param args Entrada padrão do menu principal
-	 */
+
+    /**
+     * Método para simular interface para invocar métodos sobre inscrição de Voluntarios em Ações.
+     *
+     * @throws IOException			Se houver problema na Main ou ao utilizar
+     *                              o banco de dados.
+     * @throws InterruptedException Se houver problema na Main ou ao utilizar
+     *                              o banco de dados.
+     * @throws ExecutionException   Se houver problema na Main ou ao utilizar
+     *                              o banco de dados.
+     */
 	public static void simuladorInterface() throws InterruptedException, ExecutionException, IOException {
 		Acao acao;
 		List<Acao> acoes = AcaoDAO.consultaTodasAcoes();
 		System.out.println("Ações disponíveis para inscrever:");
 
-		for(int cont = 0; cont < acoes.size(); cont++) {
+		for (int cont = 0; cont < acoes.size(); cont++) {
 			acao = acoes.get(cont);
-			System.out.println(cont + " - Ação: " + acao.getNomeAcao() + " Promotor: " + acao.getNomePromotor());
+			System.out.println(cont + " - Ação: " + acao.getNomeAcao() + " Promotor: "
+								+ acao.getNomePromotor());
 		}
-		
+
 		System.out.println("\nEscolha o número da ação ao qual deseja se cadastrar:");
-		
+
 		int option = scanner.nextInt();
 		scanner.nextLine();
-		
+
 		InscreverService.inscreverEmAcao(acoes.get(option));
 	}
-	
 }
