0009502c36568538f1217e2f6ff16e58f5918d77
Octavio Lo Leggio <ottoleggio@gmail.com>
2019-11-26 20:24:35 -0200

    checkstyle voluntarioREF

--- src/main/java/br/com/ligacao/persistence/model/VoluntarioREF.java
+++ src/main/java/br/com/ligacao/persistence/model/VoluntarioREF.java
@@ -3,28 +3,48 @@ package br.com.ligacao.persistence.model;
 import java.util.Objects;
 
 import com.google.cloud.firestore.DocumentReference;
-
+/**
+ * Classe responsável por gerenciar os dados de referência
+ * de voluntários cadastrados em ação no banco Firestore.
+ */
 public class VoluntarioREF {
+    /**
+     * Atributo da referência ao objeto Voluntario no banco.
+     */
     private DocumentReference referencia;
 
+    /**
+     * Método construtor da classe.
+     * @param referencia path da referência.
+     */
     public VoluntarioREF(DocumentReference referencia) {
         this.referencia = referencia;
     }
-    
+
+    /**
+     * Método construtor da classe.
+     */
     public VoluntarioREF() {
     }
 
+    /**
+     * Método get do atributo referência.
+     * @return Path da referência.
+     */
     public DocumentReference getReferencia() {
         return referencia;
     }
 
-    public void setReferencia(DocumentReference referencia) {
+    /**
+     * Método set do atributo referência.
+     * @param referencia path da referência.
+     */
+    public void setReferencia(final DocumentReference referencia) {
         this.referencia = referencia;
     }
-    
+
     @Override
     public int hashCode() {
-      return Objects.hash( referencia);
+      return Objects.hash(referencia);
   }
-    
 }
